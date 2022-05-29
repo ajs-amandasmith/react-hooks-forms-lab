9907f8b8348625f9a159607787381726a15d718e
Amanda Smith <ajs.amandasmith@gmail.com>
2022-05-29 16:36:10 -0500

    Pass the tests

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+
+function newItem(e) {
+  e.preventDefault();
+  // const newItem = ;
+  return onItemFormSubmit({
+    id: uuid(),
+    name: itemName,
+    category: itemCategory,
+  })
+}
+
+function handleItemName(event) {
+  setItemName(event.target.value);
+}
+
+function handleItemCategory(event) {
+  setItemCategory(event.target.value);
+}
+    
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={newItem}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+          type="text" 
+          name="name" 
+          value={itemName}
+          onChange={handleItemName}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={itemCategory} onChange={handleItemCategory}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
