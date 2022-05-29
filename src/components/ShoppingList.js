175174e18a30cb61049c84d583ef740c26372d39
Amanda Smith <ajs.amandasmith@gmail.com>
2022-05-29 15:51:15 -0500

    commit tree d01e889ed82c5b5084ffbf449bd1ff48d1ea4d9e

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,43 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(newSearch) {
+    setSearch(newSearch);
+  }
 
-    return item.category === selectedCategory;
+
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  }).filter((item) => {
+    if (search === '') return true;
+    return item.name.toLowerCase().includes(search.toLowerCase());
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+        // itemName={itemName} 
+        onItemFormSubmit={onItemFormSubmit} 
+        // itemCategory={itemCategory}
+        // onItemNameChange={handleItemName}
+        // onItemCategoryChange={handleItemCategory}
+        // items={items}
+      />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search={search}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
@@ -30,3 +50,5 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+// const itemsToDisplay = items.filter((item) => { your first filter if statement }).filter((item) => { your second filter if statement })
